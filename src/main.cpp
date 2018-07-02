22a22f958318f58b5dd511f1adab8c4e450472a5
d4v3s <david_franklin2007@hotmail.com>
2018-07-02 20:53:32 -0300

    add main para testes

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include "funcionario.h"
+#include "tratador.h"
+int main(){
+	//Funcionario(int id_, string nome_, string cpf_, int idade_,  string tipoSanguineo_, char fatorRH_,string especialidade_);
+	Funcionario vet = new Funcionario(1, "joao", "45644545", 25,  "o+", "+","sem especialidade");
+	//Veterinario vet2;
+	//std::cin >> vet2;
+	//std::cout << vet << std::endl;
+	//std::cout << vet2 << std::endl;
+	return 0;
+}
