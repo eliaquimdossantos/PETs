985064a391fb2968045de7b4e5e5da4bd0946418
Eliaquim dos Santos <eliaquimdossantos@gmail.com>
2018-07-03 18:33:41 -0300

    Atualização dos cabeçalhos

--- include/funcionario.h
+++ include/funcionario.h
@@ -1,54 +1,103 @@
-#ifndef FUNCIONARIO_H
-#define FUNCIONARIO_H
-#include <string>
+#ifndef _FUNCIONARIO_H_
+#define _FUNCIONARIO_H_
+#include <ctype.h>
 #include <iostream>
-
-using std::string;
-
-class Funcionario{	
-public:
-	Funcionario(int id_, string nome_, string cpf_, int idade_,  string tipoSanguineo_, char fatorRH_,
-				string especialidade_);
-
-	~Funcionario();
+#include <fstream> 		
+#include <vector>
+#include <memory>
+#include <iomanip>
+#include <string>
 
 
-//# protegido
-protected:
-	int id;
-	string nome;
-	string cpf;
-	int idade;
-	string tipoSanguineo;
-	char fatorRH;
-	string especialidade;
+/// funcionario is the main classs.
+class funcionario
+{	
+	/// Pubics members
+	public:
+		funcionario();
+		funcionario(std::string Id_, std::string funcao_, std::string nome_, std::string cpf_, std::string idade_, std::string tipo_, std::string fator_,
+			std::string espec_);
 
-private:
+		virtual ~funcionario();
+
+		void printFunc( std::vector<funcionario> Vet_);
+		int converte( char ch_ );
+		void carrega( void );
+		void grava( std::vector<funcionario> Vet_);
+		void controla( int tipo);
+
+
+	/// Protected members	
+	protected:
+		std::string Id;
+		std::string funcao;
+		std::string nome;
+		std::string cpf;
+		std::string idade;
+		std::string tipo;
+		std::string fator;
+		std::string espec;
+
+	private:
+		std::vector<funcionario> emprega;//<! store data.
+		
+	public:
+		/// getters
+		std::string getId();
+		std::string getfuncao();
+		std::string getnome();
+		std::string getcpf();
+		std::string getidade();
+		std::string gettipo();
+		std::string getfator();
+		std::string getespec();
+		
+		/// Setters
+		void setId(std::string Id_);
+		void setfuncao(std::string funcao_);
+		void setnome(std::string nome_);
+		void setcpf(std::string cpf_);
+		void setidade(std::string idade_);
+		void settipo(std::string tipo_);
+		void setfator(std::string fator_);
+		void setespec(std::string espec_);
+		
+	///
+	friend std::istream& operator>> (std::istream& is, funcionario& Func)
+	{
+		if(getline(is, Func.Id, ';')) 
+		{
+
+			getline(is, Func.funcao, ';');
+		    getline(is, Func.nome, ';');
+		    getline(is, Func.cpf, ';');
+		    getline(is, Func.idade, ';');
+		    getline(is, Func.tipo, ';');
+		    getline(is, Func.fator, ';');
+			getline(is, Func.espec );
+			    
+	    }
+	    return is;	
+	}
 	
-	
-public:
-	// getters
-	   int getId();
-	string getNome();
-	string getCpf();
-	   int getIdade();
-	 string getTipoSanguineo();
-	  char getFatorRH();
-	string getEspecialidade();
-	
-	// Setters
-	void setId(int id_);
-	void setNome(string nome_);
-	void setCpf(string cpf_);
-	void setIdade(int idade_);
-	void setTipoSanguineo(string tipoSanguineo_);
-	void setFatorRH(char fatorRH_);
-	void setEspecialidade(string especialidade_);
 
-	/**
-	 *@brief Sobrecarga do operador de inserção
-	 */
-	friend std::ostream& operator<< (std::ostream& o, Funcionario& f);
+	/// Function that overwrites the operator ==.
+	friend bool operator== (funcionario &a1, funcionario &a2)
+	{
+	   	return (a1.cpf == a2.cpf );
+    }
+
+    /// Function that overwrites the operator <<.
+    friend std::ostream& operator << (std::ostream& os, funcionario& Func)
+	{
+
+	     os << Func.Id << ";"<< Func.funcao << ";"<< Func.nome << ";"<< Func.cpf << ";"
+		<< Func.idade << ";"<< Func.tipo << ";"<< Func.fator << ";"<< Func.espec;
+	    	
+	    return os;
+	}
+
+	
 };
 
  
